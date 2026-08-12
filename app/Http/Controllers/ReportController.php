04c1f2421006d6bd8df00234e2d10a4a45a0b2ca
weoxz <weoxz2007xd@gmail.com>
2026-08-11 20:32:36 -0600

    fix: asigna servicios inyectados en constructores de Report y Tee

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -14,6 +14,9 @@ class ReportController extends Controller
 
     public function __construct(InstitutionService $institutionService, ElectionService $electionService, VoteTallyService $voteTallyService)
     {
+        $this->institutionService = $institutionService;
+        $this->electionService = $electionService;
+        $this->voteTallyService = $voteTallyService;
     }
 
     public function index()
