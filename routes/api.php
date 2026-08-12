483a071081e09dad29d45e9583945d86d755947b
weoxz <weoxz2007xd@gmail.com>
2026-08-11 20:32:49 -0600

    feat: expone rutas API de urnas, partidos, candidatos y votos

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UrnaController;
+use App\Http\Controllers\ResultController;
+use App\Http\Controllers\PartyController;
+use App\Http\Controllers\CandidatoController;
+use App\Http\Controllers\VoteController;
 
-Route::middleware('auth:sanctum')->group(function () {
-    //
+Route::middleware('auth:sanctum')->get(function (Request $request) {
+    return $request->user();
 });
+
+Route::post('/urnas/activar', [UrnaController::class, 'activar'])->name('urnas.activar');
+Route::post('/urnas/desactivar', [UrnaController::class, 'desactivar'])->name('urnas.desactivar');
+
+Route::get('/ganador', [ResultController::class, 'apiVerificarGanador'])->name('resultados.ganador');
+
+Route::get('/partidos', [PartyController::class, 'index'])->name('partidos.index');
+Route::get('/candidatos', [CandidatoController::class, 'index'])->name('candidatos.index');
+
+route::post('/votar', [VoteController::class, 'store'])->name('votos.store');
