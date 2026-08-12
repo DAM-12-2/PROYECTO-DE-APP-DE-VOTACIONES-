3f891b6dc5d85dbdabfe2dec1766ec54e2143e28
weoxz <weoxz2007xd@gmail.com>
2026-08-11 20:32:39 -0600

    feat: agrega validaciones y autorizacion a los form requests

--- app/Http/Requests/StoreCandidatoRequest.php
+++ app/Http/Requests/StoreCandidatoRequest.php
@@ -8,9 +8,15 @@ class StoreCandidatoRequest extends FormRequest
 {
     public function authorize(): bool
     {
+        return true;
     }
 
     public function rules(): array
     {
+        return [
+            'nombre' => 'required|string|max:255',
+            'partido_id' => 'required|exists:partidos,id',
+            'puesto_id' => 'required|exists:puesto,id',
+        ];
     }
 }
