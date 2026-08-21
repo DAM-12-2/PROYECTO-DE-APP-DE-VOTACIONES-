41deef891d17d49588f833eb6dfc4972d71cb482
Emma-321 <dereckmontes414@gmail.com>
2026-08-20 20:17:25 -0600

    feat: agrega rutas api para estudiantes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
 
 Route::middleware('auth:sanctum')->group(function () {
     //
 });
+
+// 📌 Rutas del Módulo de Estudiantes (CSV y Búsqueda)
+Route::get('/students', [StudentController::class, 'index']);
+Route::post('/students', [StudentController::class, 'store']);
+Route::put('/students/{id}', [StudentController::class, 'update']);
+Route::delete('/students/{id}', [StudentController::class, 'destroy']);
+Route::post('/students/import', [StudentController::class, 'import']);
+Route::get('/students/export', [StudentController::class, 'export']);
+Route::get('/students/search', [StudentController::class, 'search']);
